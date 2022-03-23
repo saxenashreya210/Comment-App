14b7e163c39e16d4b576d3a44df26b2ced0d8bdd
saxenashreya210 <saxenashreya210@gmail.com>
2022-03-24 00:42:31 +0530

    food app frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FeedbackpageComponent } from './feedbackpage/feedbackpage.component';
 import { ForgetpasswordComponent } from './forgetpassword/forgetpassword.component';
+import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import {NavbarComponent} from './navbar/navbar.component';
 
 const routes: Routes = [
   {
     path: "",
-    redirectTo: "sign-up",
+    redirectTo: "home",
     pathMatch: "full",
 
+  },
+  {
+    path:'home',
+    component:HomeComponent
   },
   {
     path: 'sign-up',
@@ -30,25 +36,12 @@ const routes: Routes = [
   {
     path:'feedback',
     component: FeedbackpageComponent
+  },
+  {
+    path:'navbar',
+    component: NavbarComponent
   }
-  // {
-  //   path:'',
-  //   component:SignUpPageModule,
-  //   children: [
-  //     { path: 'signin', component: SignInPageModule }
-  //   ]
-  // },
   
-  // {
-  //   path: 'signin',
-  //   component: SignInPageModule 
-  // }
-  // ,
-  // {
-  //   path: "sign-in",
-  //   loadChildren: () =>
-  //     import("../app/sign-in/sign-in.module").then((m) => m.SignInPageModule),
-  // },
   
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 import { ForgetpasswordComponent } from './forgetpassword/forgetpassword.component';
 import { FeedbackpageComponent } from './feedbackpage/feedbackpage.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [AppComponent, SignupComponent, SigninComponent, ForgetpasswordComponent, FeedbackpageComponent],
-  imports: [BrowserModule, AppRoutingModule,HttpClientModule],
+  declarations: [AppComponent, SignupComponent, SigninComponent, ForgetpasswordComponent, FeedbackpageComponent, HomeComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule,HttpClientModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
